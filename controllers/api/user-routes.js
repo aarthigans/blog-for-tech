a1a3a40b0c81e58ffb4eb6ba4cda154021bd138f
Lakshmi <mlakshmi.76@gmail.com>
2022-08-08 22:41:08 -0400

    some more files added

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+
+// Get all users
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: {
+      exclude: ["password"],
+    },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// Create user
+router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
