f163736fe5ac254d62d4b1bde1cbc6795934fb2b
Lakshmi <mlakshmi.76@gmail.com>
2022-08-09 13:47:46 -0400

    updated the js files with the script

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,103 +1,97 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const {
-    User,
-    Post,
-    Comment
-} = require('../models');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { User, Post, Comment } = require("../models");
 
-
-router.get('/', (req, res) => {
-    Post.findAll({
-            attributes: [
-                'id',
-                'title',
-                'content',
-                'created_at'
-            ],
-            include: [{
-                    model: Comment,
-                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                    include: {
-                        model: User,
-                        attributes: ['username']
-                    }
-                },
-                {
-                    model: User,
-                    attributes: ['username']
-                }
-            ]
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) =>
+        post.get({
+          plain: true,
         })
-        .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({
-                plain: true
-            }));
+      );
 
-            res.render('homepage', {
-                posts
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
+      res.render("homepage", {
+        posts,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({
+          message: "No post found with this id",
         });
+        return;
+      }
+
+      const post = dbPostData.get({
+        plain: true,
+      });
+
+      res.render("single-post", {
+        post,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.get('/post/:id', (req, res) => {
-    Post.findOne({
-            where: {
-                id: req.params.id
-            },
-            attributes: [
-                'id',
-                'title',
-                'content',
-                'created_at'
-            ],
-            include: [{
-                    model: Comment,
-                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                    include: {
-                        model: User,
-                        attributes: ['username']
-                    }
-                },
-                {
-                    model: User,
-                    attributes: ['username']
-                }
-            ]
-        })
-        .then(dbPostData => {
-            if (!dbPostData) {
-                res.status(404).json({
-                    message: 'No post found with this id'
-                });
-                return;
-            }
+router.get("/login", (req, res) => {
+  // if (req.session.loggedIn) {
+  //     res.redirect('/');
+  //     return;
+  // }
 
-            const post = dbPostData.get({
-                plain: true
-            });
-
-            res.render('single-post', {
-                post
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+  res.render("login");
 });
 
-router.get('/login', (req, res) => {
-    res.render('login');
+router.get("*", (req, res) => {
+  res.status(404).send("Can't go there!");
+  // res.redirect('/');
 });
-
-router.get('*', (req, res) => {
-    res.redirect('/');
-})
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,6 +10,16 @@ const hbs = exphbs.create({
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+const sess = {
+  secret: process.env.DB_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -24,10 +34,10 @@ app.use(
   })
 );
 app.use(routes);
+app.use(session(sess));
 
 sequelize.sync();
 
-// sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
